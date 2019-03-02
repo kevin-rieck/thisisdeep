bc8d5ce739382a642645208304fdc2e0d4cd8d9b
Kevin Rieck <clyro.isomer@gmail.com>
2019-03-02 16:29:44 +0100

    Completed ANN

--- ann.py
+++ ann.py
@@ -1,12 +1,10 @@
-# import tensorflow
-# import theano
-import keras
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from keras.models import Sequential
+from keras.layers import Dense
 
 file_path = r'.\Churn_Modelling.csv'
 df = pd.read_csv(file_path)
@@ -33,4 +31,19 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-pass
+# Building ANN
+ann = Sequential()
+ann.add(Dense(input_dim=X_train.shape[1], output_dim=6, init='uniform', activation='relu'))
+ann.add(Dense(output_dim=6, init='uniform', activation='relu'))
+ann.add(Dense(output_dim=1, init='uniform', activation='sigmoid'))
+ann.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Fitting to training data
+ann.fit(X_train, y_train, batch_size=10, epochs=100)
+
+# Predicting test data
+y_pred = ann.predict(X_test)
+y_pred = (y_pred > 0.5).astype(int)
+# Evaluating confusion matrix
+metrics = confusion_matrix(y_test, y_pred)
+print(metrics)
